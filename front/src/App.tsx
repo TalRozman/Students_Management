0f06ded4b988f105269bfe9ab836298eef7f0778
Tal Rozman <117648085+TalRozman@users.noreply.github.com>
2023-01-25 11:40:26 +0200

    Final commit
    Added SRC and Public folders for grading

---
+++ front/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Students from './features/Students/Students';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Students/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
